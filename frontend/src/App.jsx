b91fcc1c2482f8de3fd258ad18bbe4e4146ce7f2
Anji515 <anji.gd17@gmail.com>
2023-05-05 11:27:04 +0530

    Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
-import Navbar from './Components/Navbar';
+import Navbar from './Components/NavBarF';
+import NavTop from './Components/NavTop';
 import SingleProduct from './Pages/Singleproductpage';
 
 function App() {
   return (
     <div className="App">
       {/* <h1>Homepage</h1> */}
-      <Navbar/>
+       <NavTop/>
+       <Navbar/>
       <h1>Homepage</h1>
       <SingleProduct/>
     </div>

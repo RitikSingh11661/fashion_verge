afb30781e53046615b345662ba00e65c50123e37
Anji515 <anji.gd17@gmail.com>
2023-05-09 13:45:22 +0530

    Product Page

--- frontend/src/Pages/Singleproductpage.jsx
+++ frontend/src/Pages/Singleproductpage.jsx
@@ -74,7 +74,7 @@ useEffect(()=>{
     <Box display={'flex'} w='100%' m='auto' justifyContent={'center'}>
         <Text fontSize={['10px','10px','14px','16px','16px']}><Link to='/'>Home</Link></Text>
         <Text fontSize={['10px','10px','14px','16px','16px']} mt='3px'><IoIosArrowForward/></Text>
-        <Text fontSize={['10px','10px','14px','16px','16px']}><Link to='/product'>all men products</Link></Text>
+        <Text fontSize={['10px','10px','14px','16px','16px']}><Link to='/products'>all men products</Link></Text>
         <Text fontSize={['10px','10px','14px','16px','16px']} mt='3px'><IoIosArrowForward/></Text>
         <Text fontSize={['10px','10px','14px','16px','16px']} textTransform={'lowercase'}>{data.name}</Text>
       </Box>
